81727fc7729ca898872f4914da17ed683ade0978
kevinwg02. <kevin.waag@outlook.>
2024-03-12 15:01:34 +0100

    ajout de consolelogcolor

---
+++ consolecolor/consolelogtheme.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+
+const log = console.log;
+chalk.level = 1;
+log(chalk.yellow('welcome to yellow'));
+
+log(chalk.green('welcome to green'));
+
+
+
+
+
+
+
+
+
+
+
+console.log('\x1b[33m Welcome to the app! \x1b[0m');
+
